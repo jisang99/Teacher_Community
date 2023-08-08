fb7519ca3506933771aa6c48d1432336cf36e5c6
rightnowjyk <rightnotjyk>
2023-08-08 09:31:49 +0900

    model

--- teacher_community/models.py
+++ teacher_community/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from noticeboard.models import Post
+from django.utils import timezone
+from django.contrib.humanize.templatetags.humanize import intcomma
 
 #메인페이지 hot게시물은 조회 수 높은 순
 class HotWrite(models.Model):
@@ -31,3 +33,15 @@ class ThinkWrite(models.Model):
 
     class Meta:
         ordering = ['-post__created_at']  # created_at 필드를 기준으로 내림차순 정렬
+
+class Funding(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=100, decimal_places=0)  # 펀딩 금액을 나타내는 필드
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def formatted_amount(self):
+        return intcomma(self.amount)
+
+    def __str__(self):
+        return f"{self.title} - {self.formatted_amount()}원 ({self.created_at.strftime('%Y-%m-%d %H:%M:%S')})"
