187749207d123dc771c41fd707d41b77343d4a08
rightnowjyk <rightnotjyk>
2023-08-09 14:48:48 +0900

    찐막

--- mypage/admin.py
+++ mypage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MyWrite, MyReply
 
-# Register your models here.
+@admin.register(MyWrite)
+class MyWriteAdmin(admin.ModelAdmin):
+    list_display = ('post',)
+
+@admin.register(MyReply)
+class MyReplyAdmin(admin.ModelAdmin):
+    list_display = ('comment',)
--- teacher_community/admin.py
+++ teacher_community/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Funding
 
-# Register your models here.
+@admin.register(Funding)
+class FundingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'formatted_amount', 'created_at')
+    search_fields = ('title',)
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
+    def formatted_amount(self, obj):
+        return obj.formatted_amount()
+
+    formatted_amount.short_description = 'Amount'
+
+    # formatted_amount 함수를 사용하여 계산된 필드를 어드민 목록에서 표시합니
