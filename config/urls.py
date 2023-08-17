c72c8d7577c378bfc8c5a81a51b6842570f49438
soyuly <soyul8147@gmail.com>
2023-08-17 23:05:05 +0900

    fix: static 경로 수정

--- config/urls.py
+++ config/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('', include('teacher_community.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
-
 ]
 
 if settings.DEBUG:
