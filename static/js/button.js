3a4d35ed22c66eb235f42142b56fd32f0efc7845
parkkyeonghoon <134292978+parkkyeonghoon@users.noreply.github.com>
2023-08-17 20:00:59 +0900

    수정

--- static/js/button.js
+++ static/js/button.js
@@ -1,18 +1,3 @@
 
 
 
-const iconButton = document.getElementById('like_Btn');
-const icon = document.getElementById('heart_icon');
-let isRed = false;
-
-iconButton.addEventListener('click', () => {
-    if (isRed) {
-        // 아이콘 색을 원래 색상으로 변경
-        icon.style.color = '#ff0000';
-        isRed = false;
-    } else {
-        // 아이콘 색을 빨간색으로 변경
-        icon.style.color = '#ff0000';
-        isRed = true;
-    }
-});
